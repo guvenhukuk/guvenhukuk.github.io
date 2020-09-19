b30d0a4744f1e9229adea462dceae3b1f257880f
guvenhukuk <guvenzulkif@gmail.com>
2020-09-19 15:38:14 +0300

    RefreshSiteV2

---
+++ precache-manifest.73dd4d64d1b8dcc6c16b3f0c7a4a640f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de1f4332bc735fd6b926f4841131aa62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "084d75897686b3b1a3c3",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "74ffb0b513b6b74ade3c",
+    "url": "/static/css/main.7453f7b9.chunk.css"
+  },
+  {
+    "revision": "084d75897686b3b1a3c3",
+    "url": "/static/js/2.f01bf86f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f01bf86f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74ffb0b513b6b74ade3c",
+    "url": "/static/js/main.4ee94483.chunk.js"
+  },
+  {
+    "revision": "971ca2a36c34f131d50abe99934c668c",
+    "url": "/static/js/main.4ee94483.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1af6ee38465143d607d5",
+    "url": "/static/js/runtime-main.d653ab47.js"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/static/media/fontawesome-webfont.25a32416.eot"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/static/media/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "d7c639084f684d66a1bc66855d193ed8",
+    "url": "/static/media/fontawesome-webfont.d7c63908.svg"
+  },
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/static/media/fontawesome-webfont.e6cf7c6e.woff2"
+  }
+]);
