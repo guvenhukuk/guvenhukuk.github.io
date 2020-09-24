b9c7b84f7dd026c1e7c1dbc75cd6d910211276b4
guvenhukuk <guvenzulkif@gmail.com>
2020-09-24 14:39:41 +0300

    RefreshSiteV3

---
+++ precache-manifest.f0cca37477ee7295375559d1714bc4c6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "314d02fdd26491813895a9acf3e6ccb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11b856e9f84b2a69a165",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e3a35d8accb5f6a44768",
+    "url": "/static/css/main.7453f7b9.chunk.css"
+  },
+  {
+    "revision": "11b856e9f84b2a69a165",
+    "url": "/static/js/2.6b911dc7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6b911dc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3a35d8accb5f6a44768",
+    "url": "/static/js/main.570304ea.chunk.js"
+  },
+  {
+    "revision": "971ca2a36c34f131d50abe99934c668c",
+    "url": "/static/js/main.570304ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1af6ee38465143d607d5",
+    "url": "/static/js/runtime-main.d653ab47.js"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/static/media/fontawesome-webfont.25a32416.eot"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/static/media/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "d7c639084f684d66a1bc66855d193ed8",
+    "url": "/static/media/fontawesome-webfont.d7c63908.svg"
+  },
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/static/media/fontawesome-webfont.e6cf7c6e.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.73dd4d64d1b8dcc6c16b3f0c7a4a640f.js"
+  "/precache-manifest.f0cca37477ee7295375559d1714bc4c6.js"
 );
 
 self.addEventListener('message', (event) => {
